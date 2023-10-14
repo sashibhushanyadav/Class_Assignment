b5ecb63a9a8cf0e2a7c1d35fe27e00079a7f4ed6
sashibhushanyadav <contact.sashibhushan@gmail.com>
2023-10-15 00:18:10 +0545

    feat: Conditional

---
+++ Conditional op/Scripts/index.js
@@ -0,0 +1,26 @@
+// let primeNumber = prompt("Enter your number")
+
+// const num = Number(primeNumber)
+
+// let oddEvenNumber;
+// if(num%2===0){
+//     oddEvenNumber='Even'
+// }else{
+//     oddEvenNumber='Odd'
+// }
+// if(num/1===num&&num/num===1&&num%2,3,4,5,6,7!=0){
+//     console.log('The given number '+num+ ' is prime number as well as '+oddEvenNumber+' number')
+// }else{
+//     console.log('The given number '+num+ ' is not a prime number but it`s a '+oddEvenNumber+' number')
+// }
+
+let sumSeris = prompt('Enter number between 1-100')
+
+const num = Number(sumSeris)
+
+function sumOfSeries(n) {
+    const okay = n*(n + 1);
+    const sumSeriesAll = okay / 2;
+    console.log(sumOfSeries);
+}
+sumOfSeries(num);
