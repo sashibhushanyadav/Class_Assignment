eddf2355431abb6a907483fcf5d91805a9d315a1
sashibhushanyadav <contact.sashibhushan@gmail.com>
2023-10-26 00:36:16 +0545

    feat:function

---
+++ Function Practice/Scripts/index.js
@@ -0,0 +1,9 @@
+let number = prompt("Enter The Number Between 1 to 100");
+let num = Number(number);
+
+function sumOfSeries(a) {
+// let sum =(a*a+a)/2
+console.log((a*(a+1))/2)
+}
+// console.log("sf");
+sumOfSeries(num);
