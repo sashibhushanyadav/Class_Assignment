cc4e73d636acd5340af2c781c4ef33136d7b3429
sashibhushanyadav <contact.sashibhushan@gmail.com>
2023-10-28 08:16:50 +0545

    feat:UI

--- Function Practice/Scripts/index.js
+++ Function Practice/Scripts/index.js
@@ -2,8 +2,6 @@ let number = prompt("Enter The Number Between 1 to 100");
 let num = Number(number);
 
 function sumOfSeries(a) {
-// let sum =(a*a+a)/2
-console.log((a*(a+1))/2)
+document.write((a*(a+1))/2)
 }
-// console.log("sf");
 sumOfSeries(num);
