8a2567c941ef9a61a514f51e8e73cb8c297010e9
sashibhushanyadav <contact.sashibhushan@gmail.com>
2023-10-28 08:22:33 +0545

    feat:show the sum of series number on UI

--- Function Practice/Scripts/index.js
+++ Function Practice/Scripts/index.js
@@ -2,6 +2,8 @@ let number = prompt("Enter The Number Between 1 to 100");
 let num = Number(number);
 
 function sumOfSeries(a) {
-document.write((a*(a+1))/2)
+  document.write(
+    "The sum of series number between 1 -100 is " + (a * (a + 1)) / 2
+  );
 }
 sumOfSeries(num);
